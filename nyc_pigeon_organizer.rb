543f3d98baf7f7f2f20306eba142886d846f0efb
Eddie Lopez <iameddielopez@gmail.com>
2020-07-23 19:53:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,6 +4,7 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   data.each_with_object({}) do |(key,value), final_array|
     value.each do |inner_key, names|
+      names.each do |names|
     
     binding.pry
   end
